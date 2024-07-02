2712858bdab8d04cc7729d4de203a82408b457f0
RedStone576 <RedStone576@users.noreply.github.com>
2024-07-02 08:56:01 +0000

    deploy: 5f35307d5e2811daccecb408a65b10ef1b3daa65

--- js/binding.js
+++ js/binding.js
@@ -12,7 +12,6 @@ $("#tools-picker-E").addEventListener("click", () => setCurrent("E"));
 function setCurrent(p) {
     current = p;
     console.log(current);
-    // ass
     for (const x of ["Z", "L", "O", "S", "I", "J", "T", "G", "D", "E"]) {
         if (p === x) {
             $(`#tools-picker-${x}`).classList.add("picker-highlight");
@@ -43,7 +42,6 @@ function draw(x, y) {
     const element = $(`#editor-board-column-${y}-${x}`);
     element.className = "";
     element.classList.add(`editor-board-tile-${current}`);
-    // this is ass
     if (mapString.length !== width * height)
         mapString += "_".repeat((width * height) - mapString.length);
     const c = current
@@ -54,22 +52,26 @@ function draw(x, y) {
     mapString = mapString.substring(0, index) + c + mapString.substring(index + 1);
     $("#tools-map-string").value = mapString + (queueString ? "?" : "") + queueString;
 }
-// 
-function queueOnInput() {
+function queueOnInput(e) {
+    const { inputType } = e;
+    const selectionStart = $("#tools-queue-string").selectionStart;
     const value = $("#tools-queue-string").value.toUpperCase();
-    const filter = ["Z", "L", "O", "S", "I", "J", "T", "?"];
+    const filter = ["Z", "L", "O", "S", "I", "J", "T", "?", ","];
     let filteredQueue = value
         .split("")
         .filter(char => filter.indexOf(char) > -1)
         .join("");
-    // check delimiter
-    if (filteredQueue.split("?").length > 2) {
-        const parts = filteredQueue.split("?");
-        filteredQueue = [parts[0], parts[1]].join("?");
-    }
+    let parts = filteredQueue.split("?");
+    if (!/^[ZLOSIJT](,[ZLOSIJT])*$/.test(parts[0]))
+        parts[0] = parts[0].replaceAll(",", "").split("").join(",");
+    if (parts[1] && parts[1].length > 1)
+        parts[1] = parts[1][0];
+    filteredQueue = parts.length > 1 ? [parts[0], parts[1]].join("?") : parts[0];
     queueString = filteredQueue;
     $("#tools-queue-string").value = filteredQueue;
     $("#tools-map-string").value = mapString + (queueString ? "?" : "") + queueString;
+    const sc = inputType === "deleteContentBackward" ? (selectionStart - 1) : (selectionStart + 1);
+    $("#tools-queue-string").setSelectionRange(sc < 0 ? 0 : sc, sc < 0 ? 0 : sc);
 }
 function mapOnInput() {
     if ($("#tools-map-string").value === "") {
@@ -82,18 +84,16 @@ function mapOnInput() {
         }
     }
     const selectionStart = $("#tools-map-string").selectionStart;
-    const mapStringValue = $("#tools-map-string").value /* "{mapString}?{queueString}?{holdPiece*}" */.toUpperCase().split("?");
+    const mapStringValue = $("#tools-map-string").value.toUpperCase().split("?");
     const filter = ["Z", "L", "O", "S", "I", "J", "T", "#", "@", "_", "?"];
     let filteredMapString = mapStringValue[0]
         .split("")
         .filter(char => filter.indexOf(char) > -1)
         .join("");
-    // chop off extra char, might change this later
     if (filteredMapString.length > (width * height))
         filteredMapString = filteredMapString.slice(0, width * height);
     {
         const parts = mapStringValue.slice(1);
-        // check if there's >2 delimiter
         var queueStringValue = parts.length > 2 ? [parts[0], parts[1]] : parts;
     }
     mapString = filteredMapString.toUpperCase();
@@ -101,10 +101,8 @@ function mapOnInput() {
     $("#tools-queue-string").value = queueString;
     $("#tools-map-string").value = mapString + (queueString ? "?" : "") + queueString;
     $("#tools-map-string").setSelectionRange(selectionStart, selectionStart);
-    // draw
     const mapArray = mapString.match(new RegExp(`.{1,${width}}`, "g"));
     if (!mapArray) {
-        //init()
         return;
     }
     for (let y = 0; y < height; y++) {
--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 "use strict";
-// load this first, then binding.js, and then board.js
 const $ = (query) => document.querySelector(query);
-// # : (G)arbage
-// @ : (D)ark garbage
-// _ : (E)empty
 let current = "E";
 let mapString = "";
 let queueString = "";
